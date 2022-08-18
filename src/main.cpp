5d7f7656a09ece582cac29dade73b0d0063fc8d2
Guillaume Baptist <guillaume.baptist@free.fr>
2022-08-18 19:19:17 +0200

    after fusion of the code of max30102

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 
 #include <Arduino.h>
 #include <WiFi.h>
+#include <Wire.h>
+#include "MAX30105.h"
 
 // Wifi
 
@@ -8,12 +10,17 @@ const char *ssid = "SFR_EC58";
 //const char *password = "uguele2vocuminhonext";// parents
 const char *password = "96wwza4yfz24qhtc4mxq"; // hameau
 
+unsigned long Time;
+String Data_wifi;
+
 // to set the static IP address to 192, 168, 1, 184
 IPAddress local_IP(192, 168, 1, 70);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 WiFiServer server(80);
+
+MAX30105 particleSensor;
 
 int Wire_Not_Plugged=2000;
 
@@ -40,8 +47,19 @@ void setup()
     delay(100);
     Serial.print(".");
   }
-
+  Serial.println("Connected");
   server.begin();
+
+
+  // Initialize sensor
+  if (particleSensor.begin() == false)
+  {
+    Serial.println("MAX30102 was not found. Please check wiring/power. ");
+    while (1);
+  }
+
+  particleSensor.setup(); //Configure sensor. Use 6.4mA for LED drive
+
 }
 
 void loop()
@@ -66,10 +84,12 @@ void loop()
       {
         // send the value of analog input 0:
         //  Serial.println(analogRead(34));
-        client.println(analogRead(34));
+        //Time = millis();
+        Data_wifi = String(String(millis()) + "," + String(particleSensor.getRed()) + "," + String(particleSensor.getIR()) + "," + String(analogRead(34)));
+        client.println(Data_wifi);
       }
       //Wait for a bit to keep serial data from saturating
-      delay(0.5);
+      delay(0.1);
     }
 
     client.stop();
@@ -77,75 +97,55 @@ void loop()
   }
 }
 
-  // void setup()
-  // {
 
-  //   Serial.begin(9600);
-  //   Wire.begin();
-  //   delay(2000);
 
-  //   if (!mpu.setup(0x68))
-  //   { // change to your own address
-  //     while (1)
-  //     {
-  //       Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
-  //       delay(5000);
-  //     }
-  //   }
+/*
+  MAX30105 Breakout: Output all the raw Red/IR/Green readings
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 2nd, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
 
-  //   if (!WiFi.config(local_IP, gateway, subnet))
-  //   {
-  //     Serial.println("IP adress could not be set to 192.168.0.20");
-  //   }
+  Outputs all Red/IR/Green values.
 
-  //   WiFi.begin(ssid, password);
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
 
-  //   while (WiFi.status() != WL_CONNECTED)
-  //   {
-  //     delay(500);
-  //     // Serial.print(".");
-  //   }
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
 
-  //   server.begin();
+  This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+*/
 
-  //   mpu.setAccBias(1013.52, 19.45, 996.83);
-  //   mpu.setGyroBias(0.09, 1.55, 4.78);
-  //   mpu.setMagBias(113.95, 329.77, -416.71);
-  //   mpu.setMagScale(1.09, 0.99, 0.94);
-  //   mpu.setMagneticDeclination(2);
 
-  //   mpu.setFilterIterations(10);
-  // }
 
-  // void loop()
-  // {
 
-  //   WiFiClient client = server.available(); // listen for incoming clients
 
-  //   if (client)
-  //   { // if you get a client,
-  //     // Serial.println("New Client."); // print a message out the serial port
-  //     String currentLine = "";
 
-  //     while (client.connected())
-  //     { // loop while the client's connected
+#define debug Serial //Uncomment this line if you're using an Uno or ESP
+//#define debug SerialUSB //Uncomment this line if you're using a SAMD21
 
-  //       if (mpu.update())
-  //       {
+void setup()
+{
+  debug.begin(9600);
+  debug.println("MAX30102 Basic Readings Example");
 
-  //         currentLine = String(millis()) + "," + String(mpu.getAccX()) + "," + String(mpu.getAccY()) + "," + String(mpu.getAccZ()) +
-  //                       "," + String(mpu.getGyroX()) + "," + String(mpu.getGyroY()) + "," + String(mpu.getGyroZ()) +
-  //                       "," + String(mpu.getMagX()) + "," + String(mpu.getMagY()) + "," + String(mpu.getMagZ()) +
-  //                       "," + String(mpu.getRoll()) + "," + String(mpu.getPitch()) + "," + String(mpu.getYaw()) +
-  //                       "," + String(mpu.getQuaternionX()) + "," + String(mpu.getQuaternionY()) + "," + String(mpu.getQuaternionZ()) +
-  //                       "," + String(mpu.getQuaternionW());
 
-  //         client.println(currentLine);
-  //         delay(10);
-  //       }
-  //     }
+}
 
-  //     client.stop();
-  //     Serial.println("Client Disconnected.");
-  //   }
-  // }
+void loop()
+{
+//  debug.print(" R[");
+//  debug.print(particleSensor.getRed());
+//  debug.print("] IR[");
+//  debug.print(particleSensor.getIR());
+//  debug.print("]");
+//  debug.println(); 
+
+
+
+
+}
