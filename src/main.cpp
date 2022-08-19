b509422fed5051db7f7dca6573fd7133fe1f85a9
Guillaume Baptist <guillaume.baptist@free.fr>
2022-08-19 11:29:42 +0200

    some changes

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ const char *password = "96wwza4yfz24qhtc4mxq"; // hameau
 
 unsigned long Time;
 String Data_wifi;
+uint32_t A;
+uint32_t B;
 
 // to set the static IP address to 192, 168, 1, 184
 IPAddress local_IP(192, 168, 1, 70);
@@ -77,15 +79,21 @@ void loop()
 
       if ((digitalRead(35) == 1) || (digitalRead(32) == 1))
       {
-        Serial.println(Wire_Not_Plugged);
-        client.println(Wire_Not_Plugged);
+       // Serial.println(Wire_Not_Plugged);
+       // client.println(Wire_Not_Plugged);
       }
       else
       {
         // send the value of analog input 0:
         //  Serial.println(analogRead(34));
-        //Time = millis();
+        Time = millis();
+        A=particleSensor.getRed();
+        Serial.println(millis()-Time); 
+        B= particleSensor.getIR();
+        Serial.println(millis()-Time);  
+
         Data_wifi = String(String(millis()) + "," + String(particleSensor.getRed()) + "," + String(particleSensor.getIR()) + "," + String(analogRead(34)));
+        
         client.println(Data_wifi);
       }
       //Wait for a bit to keep serial data from saturating
@@ -97,55 +105,3 @@ void loop()
   }
 }
 
-
-
-/*
-  MAX30105 Breakout: Output all the raw Red/IR/Green readings
-  By: Nathan Seidle @ SparkFun Electronics
-  Date: October 2nd, 2016
-  https://github.com/sparkfun/MAX30105_Breakout
-
-  Outputs all Red/IR/Green values.
-
-  Hardware Connections (Breakoutboard to Arduino):
-  -5V = 5V (3.3V is allowed)
-  -GND = GND
-  -SDA = A4 (or SDA)
-  -SCL = A5 (or SCL)
-  -INT = Not connected
-
-  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-  but it will also run at 3.3V.
-
-  This code is released under the [MIT License](http://opensource.org/licenses/MIT).
-*/
-
-
-
-
-
-
-#define debug Serial //Uncomment this line if you're using an Uno or ESP
-//#define debug SerialUSB //Uncomment this line if you're using a SAMD21
-
-void setup()
-{
-  debug.begin(9600);
-  debug.println("MAX30102 Basic Readings Example");
-
-
-}
-
-void loop()
-{
-//  debug.print(" R[");
-//  debug.print(particleSensor.getRed());
-//  debug.print("] IR[");
-//  debug.print(particleSensor.getIR());
-//  debug.print("]");
-//  debug.println(); 
-
-
-
-
-}
